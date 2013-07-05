1c1f897c69c5751588cbdfb5b9ab5bc84c7d0f77
Carlos Canizal <carlos@codecery.com>
2013-07-05 14:14:00 -0500

    Showing  suits

--- blackjack_multiplayer.rb
+++ blackjack_multiplayer.rb
@@ -6,7 +6,7 @@ def start_game
 	suits = %w(Spades Diamonds Hearts Clubs)
 	values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 	deck = suits.product(values)
-	decks = [deck]
+	decks = [deck,deck,deck,deck]
 end
 
 def print_cards cards
